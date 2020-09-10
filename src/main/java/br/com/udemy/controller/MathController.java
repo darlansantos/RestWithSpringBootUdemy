d9fed403174b2f1bdf91c8fc84b0caf3301ceb6c
Darlan Parreiras Santos <darlanparreiras@gmail.com>
2020-09-09 21:38:41 -0300

    Métodos de Validação

--- src/main/java/br/com/udemy/controller/MathController.java
+++ src/main/java/br/com/udemy/controller/MathController.java
@@ -17,12 +17,17 @@ public class MathController {
 		return sum;
 	}
 
-	private Double convertToDouble(String numberOne) {
-		return 1D;
+	private Double convertToDouble(String strNumber) {
+		if (strNumber == null) return 0D;
+		String number = strNumber.replaceAll(",", ".");
+		if (isNumeric(number)) return Double.parseDouble(number);
+		return 0D;
 	}
 
-	private boolean isNumeric(String number) {
-		return false;
+	private boolean isNumeric(String strNumber) {
+		if (strNumber == null) return false;
+		String number = strNumber.replaceAll(",", ".");
+		return number.matches("[-+]?[0-9]*\\\\.?[0-9]+");
 	} 
 
 }
