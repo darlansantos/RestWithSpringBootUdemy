41bb6af7cd1afabdab4d47e5f34f9cd6824c520b
Darlan Parreiras Santos <darlanparreiras@gmail.com>
2020-09-09 22:58:05 -0300

    Tratamento de Exceções

---
+++ src/main/java/br/com/udemy/exception/handler/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,31 @@
+package br.com.udemy.exception.handler;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import br.com.udemy.exception.ExceptionResponse;
+import br.com.udemy.exception.UnsuportedMathOperationException;
+
+@RestController
+@RestControllerAdvice
+public class CustomizedResponseEntityExceptionHandler {
+	
+	@ExceptionHandler(Exception.class)
+	public final ResponseEntity<ExceptionResponse> handleAllExceptions(Exception ex, WebRequest request) {
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	@ExceptionHandler(UnsuportedMathOperationException.class)
+	public final ResponseEntity<ExceptionResponse> handleBadRequestExceptions(Exception ex, WebRequest request) {
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+	}
+
+}
