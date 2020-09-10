b757efb7c117ad48c53ad1d4f0d2484f654ac7d2
Darlan Parreiras Santos <darlanparreiras@gmail.com>
2020-09-09 21:22:10 -0300

    Implementando o Controller

--- src/main/java/br/com/udemy/controller/GreetingController.java
+++ src/main/java/br/com/udemy/controller/GreetingController.java
@@ -2,11 +2,10 @@ package br.com.udemy.controller;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import br.com.udemy.domain.Greeting;
 
 @RestController
 public class GreetingController {
@@ -15,9 +14,9 @@ public class GreetingController {
 	
 	private final AtomicLong counter = new AtomicLong();
 	
-	@RequestMapping("/greeting")
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "world") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	@RequestMapping(value = "/sum/numberOne/numberTwo", method = RequestMethod.GET)
+	public Double sum(@PathVariable("numberOne") String nnumberOneame, @PathVariable("numberTwo") String numberTwo) {
+		return 1D;
 		
 	} 
 
