4c547d21efe5b8440e76c15a40e6bb466a974dc9
Darlan Parreiras Santos <darlanparreiras@gmail.com>
2020-09-21 21:47:40 -0300

    Habilitando CORS de forma global

--- src/main/java/br/com/udemy/controller/PersonController.java
+++ src/main/java/br/com/udemy/controller/PersonController.java
@@ -7,7 +7,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -40,7 +39,7 @@ public class PersonController {
 		persons.stream().forEach(p -> p.add(linkTo(methodOn(PersonController.class).findById(p.getKey())).withSelfRel()));
 		return persons;
 	}
-	@CrossOrigin(origins = "http://localhost:8080")
+	//@CrossOrigin(origins = "http://localhost:8080")
 	@ApiOperation(value = "Find a specific person by your ID")
 	@GetMapping(value = "/{id}", produces = { "application/json", "application/xml", "application/x-yaml" })
 	@ResponseStatus(HttpStatus.OK)
@@ -50,7 +49,7 @@ public class PersonController {
 		return personVO;
 	}
 
-	@CrossOrigin(origins = {"http://localhost:8080", "https://www.google.com"})
+	//@CrossOrigin(origins = {"http://localhost:8080", "https://www.google.com"})
 	@ApiOperation(value = "Create a new person")
 	@PostMapping(produces = { "application/json", "application/xml", "application/x-yaml" }, consumes = { "application/json", "application/xml", "application/x-yaml" })
 	@ResponseStatus(HttpStatus.CREATED)
